d72119a723cf2bc561672344be67cc6da9acc3ca
EUKILLER <142009137+EUKILLER@users.noreply.github.com>
2023-08-18 15:47:12 +0500

    Add files via upload

---
+++ PROJECT3/app.js
@@ -0,0 +1,10 @@
+var name = prompt("ENTER YOUR NAME")
+var num1 = +prompt("ENTER YOUR MATH NUMBER")
+var num2 = +prompt("ENTER YOUR URDU NUMBER")
+var num3 = +prompt("ENTER YOUR ENLISH NUMBER")
+
+// document.write(num1 + num2 + num3)
+
+// document.write()
+
+// // document.write(num1+num2+num3/200*100)
